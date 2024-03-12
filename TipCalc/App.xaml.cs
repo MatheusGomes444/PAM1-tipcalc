44fce810ec0b2025c81a571c74b207a9cc21b105
Matheus Gomes <aquersaber@gmail.com>
2024-03-11 23:02:54 -0300

    feat: add calc

---
+++ TipCalc/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TipCalc
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TipCalc/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿namespace TipCalc
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+      
+        }
+        private void SliderTipePercent_ValueChanged(object sender, ValueChangeEventArgs eventArgs e)
+        {
+            LabelPercentageValue.Text = Math.Round(SliderTipePercent.Value).ToString();
+        }
+
+        private void BTN1_Clicked(object sender, EventArgs e)
+        {
+            SliderTipePercent.Value = 15;
+        }
+
+        private void BTN2_Clicked(object sender, EventArgs e)
+        {
+            SliderTipePercent.Value = 20;
+        }
+
+        private void BTN3_Clicked(object sender, EventArgs e)
+        {
+            double result = CalculateTip();
+            double roundeResult = Math.Floor(result);
+            double amount = Convert.ToDouble(roundeResult);
+            double totalValue = roundeResult + amount;
+            LabelTipValue.Text = roundeResult.ToString();
+            LabelTotalValue.Text = totalValue.ToString();
+
+        }
+
+        private void BTN4_Clicked(object sender, EventArgs e)
+        {
+            double result = CalculateTip();
+            double roundeResult = Math.Ceiling(result);
+            double amount = Convert.ToDouble(roundeResult);
+            double totalValue = roundeResult + amount;
+            LabelTipValue.Text = roundeResult.ToString();
+            LabelTotalValue.Text = totalValue.ToString();
+
+        }
+        private double CalculateTip()
+        {
+            double amount = Convert.ToDouble (ValueEntry.Text.ToString());
+            double percent = SliderTipePercent.Value;
+
+            //
+            double result = amount * (percent / 100);
+            return result;
+        }
+        private void SliderTipePercent_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            LabelPercentageValue.Text =Math.Round(SliderTipePercent.Value)String();
+        }
+    }
+
+}
